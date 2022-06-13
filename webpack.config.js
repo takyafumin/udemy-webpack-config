275b4c1bbf29243d4b575b2186cf384804a7fc24
takyafumin <takyafumin@gmail.com>
2022-06-13 12:06:41 +0900

    複数HTMLファイルの作成

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,11 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: "./src/templates/index.pug",
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/templates/access.pug",
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ]
