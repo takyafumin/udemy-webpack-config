4f6744be85e8117ba9b743bf8b2c5854764a4bef
takyafumin <takyafumin@gmail.com>
2022-06-18 12:23:31 +0900

    ファイル圧縮

--- webpack.config.js
+++ webpack.config.js
@@ -47,20 +47,29 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpg)/,
+                test: /\.(png|jpg|jpeg)/,
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name][ext]',
                 },
-                // use: [
-                //     {
-                //         loader: 'file-loader',
-                //         options: {
-                //             esModule: false,
-                //             name: 'images/[name].[ext]'
-                //         }
-                //     },
-                // ]
+                use: [
+                    // {
+                    //     loader: 'file-loader',
+                    //     options: {
+                    //         esModule: false,
+                    //         name: 'images/[name].[ext]'
+                    //     }
+                    // },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            }
+                        }
+                    }
+                ]
             },
             {
                 test: /\.pug/,
